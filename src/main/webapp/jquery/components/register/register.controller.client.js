829132f2d0f9b27e574fae8d9e13ceb0d2b8f6ab
jiayiliu <liu.jiayi@husky.neu.edu>
2018-05-27 07:13:47 -0700

    Created register templete,register controller and register style sheet

---
+++ src/main/webapp/jquery/components/register/register.controller.client.js
@@ -0,0 +1,15 @@
+(function () {
+    var $usernameFld, $passwordFld, $verifyPasswordFld;
+    var $registerBtn;
+    var $loginBtn;
+    var userService = new UserServiceClient();
+    $(main);
+
+    function main() {
+    	
+    
+    }
+    
+   
+    
+})();
