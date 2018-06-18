e22e287b3e5ca49450a45ee46db3bcaa7429fad7
jiayiliu <liu.jiayi@husky.neu.edu>
2018-06-18 15:00:06 -0700

    implement findall widgets for lesson and save all widgets

--- src/main/java/com/example/myapp/models/Widget.java
+++ src/main/java/com/example/myapp/models/Widget.java
@@ -4,15 +4,65 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Widget {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-private int id;
+    private int id;
+	private String widgetType;
+	private String text;
+	private int size;
+	private String name;
+	private String link;
+	private String listType;
+	
+	  public String getListType() {
+		return listType;
+	}
+	public void setListType(String listType) {
+		this.listType = listType;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@ManyToOne
+	  @JsonIgnore
+	  private Lesson lesson;
+	
+public Lesson getLesson() {
+		return lesson;
+	}
+	public void setLesson(Lesson lesson) {
+		this.lesson = lesson;
+	}
+public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
 public int getId() {
 		return id;
 	}
+	public String getWidgetType() {
+	return widgetType;
+}
+public void setWidgetType(String widgetType) {
+	this.widgetType = widgetType;
+}
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -22,5 +72,5 @@ public int getId() {
 	public void setText(String text) {
 		this.text = text;
 	}
-private String text;
+
 }
--- src/main/java/com/example/myapp/services/WidgetService.java
+++ src/main/java/com/example/myapp/services/WidgetService.java
@@ -1,15 +1,21 @@
 package com.example.myapp.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.myapp.models.Course;
+import com.example.myapp.models.Lesson;
+import com.example.myapp.models.Module;
 import com.example.myapp.models.Widget;
+import com.example.myapp.repositories.LessonRepository;
 import com.example.myapp.repositories.WidgetRepository;
 
 @RestController
@@ -18,16 +24,38 @@ public class WidgetService {
 	@Autowired
 	WidgetRepository repository;
 	
-	@GetMapping("/api/widget")
-public List<Widget> findAllWidgets(){
-	return (List<Widget>)repository.findAll();
+	@Autowired
+	LessonRepository lessonrepository;
+	
+//	@GetMapping("/api/widget")
+//public List<Widget> findAllWidgets(){
+//	return (List<Widget>)repository.findAll();
+//}
+	@GetMapping("/api/lesson/{lessonId}/widget")
+public List<Widget> findAllWidgets(@PathVariable("lessonId") int lessonId){
+		Optional<Lesson> data = lessonrepository.findById(lessonId);
+		if (data.isPresent()) {
+			Lesson lesson = data.get();
+			return lesson.getWidgets();
+		}
+		return null;
 }
 	
-	@PostMapping("/api/widget/save")
-		public void saveAllWidgets(@RequestBody List<Widget> widgets) {
-		repository.deleteAll();
-		for (Widget widget:widgets) {
-			repository.save(widget);
+	@PostMapping("/api/lesson/{lessonId}/widget/save")
+		public void saveAllWidgets(@PathVariable("lessonId") int lessonId, @RequestBody List<Widget> widgets) {
+		Optional<Lesson> data = lessonrepository.findById(lessonId);
+		if (data.isPresent()) {
+			Lesson lesson = data.get();
+			 for (Widget widget:lesson.getWidgets()) {
+			repository.delete(widget);
+			 }
+			 
+			for (Widget newwidget:widgets) {
+			newwidget.setLesson(lesson);
+			repository.save(newwidget);
+		     }
+		
 		}
+		
 	}
 }
